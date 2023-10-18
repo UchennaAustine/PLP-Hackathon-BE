3f110ffb1207863f4abed74aa700d0b4313ed20b
UchennaAustine <uchennaaustine8@gmail.com>
2023-10-18 15:18:05 +0100

    push

--- mainApp.ts
+++ mainApp.ts
@@ -1,27 +1,25 @@
-import express,{Application, Request, Response} from "express"
-import cors from "cors"
-import morgan from "morgan"
-import helmet from "helmet"
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import user from "./router/router";
 
+export const mainApp = (app: Application) => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan("dev"));
+  app.use(helmet());
+  app.use("/api", user);
 
-export const mainApp = (app:Application)=>{
-        app.use(express.json())
-        app.use(cors())
-        app.use(morgan("dev"))
-        app.use(helmet())
-
-        app.get("/",(req:Request,res:Response)=>{
-            try {
-                return res.status(200).json({
-                    message:"api is live ❤❤❤..."
-                    
-                })
-        
-            } catch (error) {
-                return res.status(404).json({
-                    message:`api error:${error}`
-                })
-            }
-            }
-        )
-}
+  app.get("/", (req: Request, res: Response) => {
+    try {
+      return res.status(200).json({
+        message: "api is live ❤❤❤...",
+      });
+    } catch (error) {
+      return res.status(404).json({
+        message: `api error:${error}`,
+      });
+    }
+  });
+};
