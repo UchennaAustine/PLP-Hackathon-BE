0159532dc070c25a86ef043d455456765e1dbcf1
Williams-Daniel <tito4bbt@gmail.com>
2023-10-18 21:16:53 +0100

    touchRouter

--- router/router.ts
+++ router/router.ts
@@ -10,12 +10,14 @@ import {
 } from "../controller/controller";
 
 import multer from "multer";
+import validatorHolder from "../utils/validatorHolder";
+import {registerValidator}  from "../utils/validator";
 
 const router: Router = Router();
 
 const imageUpload = multer().single("avatar");
 
-router.route("/register").post(Register);
+router.route("/register").post(registerValidator,Register);
 router.route("/:token/verify").post(Verification);
 router.route("/sign-in").post(SignIn);
 router.route("/view-users").get(Users);
--- utils/validator.ts
+++ utils/validator.ts
@@ -3,7 +3,7 @@ import joi from "joi"
 
 const regex =  /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/;
 
-export const registerValidator = joi.object({
+export const registerValidator:any = joi.object({
     name:joi.string().required(),
     email:joi.string().email().lowercase().trim(),
     password:joi.string().pattern(new RegExp(regex)).required()
